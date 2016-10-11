0db6dc60a28963f4adb273722daac4536a086f82
Dmitriy Pavlukhin <padmitriy@gmail.com>
2016-10-11 19:31:10 +0300

    Update server logic + SQL

--- ru/project/server/ClientHandler.java
+++ ru/project/server/ClientHandler.java
@@ -10,11 +10,15 @@ import java.net.Socket;
  */
 public class ClientHandler implements Runnable {
     private Socket sock;
+    private MyServer owner;
     private DataInputStream in;
     private DataOutputStream out;
+    private String nick;
 
-    public ClientHandler(Socket sock) {
+    public ClientHandler(MyServer owner, Socket sock) {
+        this.owner = owner;
         this.sock = sock;
+        this.nick = "";
         try {
             in = new DataInputStream(sock.getInputStream());
             out = new DataOutputStream(sock.getOutputStream());
@@ -28,20 +32,44 @@ public class ClientHandler implements Runnable {
         try {
             while (true) {
                 String str = in.readUTF();
+                if (str != null && str.startsWith("/auth")) { // /auth login1 pass1
+                    String login = str.split(" ")[1];
+                    String pass = str.split(" ")[2];
+                    String user = SQLHandler.getNickByLoginPass(login, pass);
+                    if (user != null) {
+                        nick = user;
+                        sendMsg("abcd");
+                        break;
+                    } else sendMsg("Auth error");
+                }
+            }
+            while (true) {
+                String str = in.readUTF();
                 if (str != null) {
-                    out.writeUTF("echo: " + str);
-                    out.flush();
                     if (str.equals("/end")) break;
+                    System.out.println(nick + ": " + str);
+                    owner.broadcastMsg(nick, str);
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
             try {
+                owner.broadcastMsg("server", "Client disconnected");
+                owner.unsubscribe(this);
                 sock.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    public void sendMsg(String msg) {
+        try {
+            out.writeUTF(msg);
+            out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
